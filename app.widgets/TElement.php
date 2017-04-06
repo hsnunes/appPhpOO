ea5acfa7946c169b8f7c39bdf6941971f43e5e20
Hilder Nunes <hildersn@gmail.com>
2017-04-06 12:04:18 -0300

    Começando a aplicação de estrutura de controle e criação da classe de elementos html

---
+++ app.widgets/TElement.php
@@ -0,0 +1,85 @@
+<?php
+/**
+* Classe TElement
+* Classe para abstralçai de tags HTML
+*/
+class TElemnet
+{
+	private $name;
+	private $properties;
+	/**
+	 * Método Construtor
+	 * @param [String] $nameTag [O nome da tag a ser criada]
+	 */
+	public function __construct( $nameTag )
+	{
+		$this->name = $nameTag;
+	}
+	/**
+	 * Método __set()
+	 * Interceptará as atribuições à propriedades do objeto
+	 * @param $[name] [Nome da propriedade]
+	 * @param $[value] [Valor da propriedade]
+	 */
+	public function __set( $name, $value )
+	{
+		$this->propriedade[$name] = $value;
+	}
+	/**
+	 * Método add()
+	 * Permite adicionar elementos filho ao objeto
+	 * @param $[child] [Objeto filho]
+	 */
+	public function add( $child )
+	{
+		$this->children[] = $child;
+	}
+	/**
+	 * Método open()
+	 * Exibe a tag de abertura
+	 */
+	private function open()
+	{
+		echo "<{$this->name}";
+		if ( $this->properties )
+		{
+			foreach ( $this->properties as $name => $value )
+			{
+				echo " {$name}=\"{$value}\"";
+			}
+		}
+		echo '>';
+	}
+	/**
+	 * Método show()
+	 * Exibe a tag na tela, juntamente com seu conteudo
+	 */
+	public function show()
+	{
+		$this->open();
+		echo PHP_EOL;
+		if ( $this->children )
+		{
+			foreach ( $this->children as $child )
+			{
+				if ( is_object( $child ) )
+				{
+					$child->show();
+				}
+				else if ( is_string( $child ) OR is_numeric( $child ) )
+				{
+					echo $chid;
+				}
+			}
+		}
+		$this->close();
+	}
+	/**
+	 * Método close()
+	 * Fecha uma tag HTML
+	 */
+	public function close()
+	{
+		echo "</{$this->name}>" , PHP_EOL;
+	}
+}
